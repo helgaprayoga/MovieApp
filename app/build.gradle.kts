7ff309af595d68a865e3737e6f2580a97111e2b9
Helga Arya Prayoga <helgaaryaprayoga@gmail.com>
2025-08-03 22:15:08 +0700

    feat: Add TopAppBar and      MainContent

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.learn.movieapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.learn.movieapp"
--- app/src/main/java/com/learn/movieapp/MainActivity.kt
+++ app/src/main/java/com/learn/movieapp/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.learn.movieapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.learn.movieapp.ui.theme.MovieAppTheme
 
@@ -18,30 +25,46 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovieAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = "MovieApp")
+                            },
+                    colors = TopAppBarDefaults.topAppBarColors(Color.Magenta)
+                )
+            }
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Surface(
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Text(text = "Hello")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MovieAppTheme {
-        Greeting("Android")
+fun AppPreview() {
+    MyApp {
+        MainContent()
     }
 }
