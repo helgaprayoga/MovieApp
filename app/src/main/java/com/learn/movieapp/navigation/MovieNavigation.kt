ffd48a9311a64caccea1d7c3825d0016dbd47e23
Helga Arya Prayoga <helgaaryaprayoga@gmail.com>
2025-08-04 00:44:40 +0700

    feat(navigation): Pass movie title as argument      to DetailsScreen

--- app/src/main/java/com/learn/movieapp/navigation/MovieNavigation.kt
+++ app/src/main/java/com/learn/movieapp/navigation/MovieNavigation.kt
@@ -1,9 +1,11 @@
 package com.learn.movieapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.learn.movieapp.screens.details.DetailsScreen
 import com.learn.movieapp.screens.home.HomeScreen
 
@@ -16,8 +18,14 @@ fun MovieNavigation() {
             HomeScreen(navController = navController)
         }
 
-        composable(MovieScreens.DetailsScreen.name) {
-            DetailsScreen(navController = navController)
+        composable(
+            MovieScreens.DetailsScreen.name+"/{movie}",
+            arguments = listOf(navArgument(name = "movie") {type = NavType.StringType})
+        ) { backStackEntry ->
+            DetailsScreen(
+                navController = navController,
+                backStackEntry.arguments?.getString("movie")
+            )
         }
     }
 }
