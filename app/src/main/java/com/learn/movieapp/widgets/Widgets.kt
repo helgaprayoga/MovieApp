beb7bdbc08a41549f607f20af3f4a83cb35c5bd6
Helga Arya Prayoga <helgaaryaprayoga@gmail.com>
2025-08-04 23:52:54 +0700

    feat: Add expandable movie plot
      Implements an expandable section for the movie plot on the
      details screen.

--- app/src/main/java/com/learn/movieapp/widgets/Widgets.kt
+++ app/src/main/java/com/learn/movieapp/widgets/Widgets.kt
@@ -1,5 +1,6 @@
 package com.learn.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,6 +14,8 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -20,13 +23,23 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import coil3.compose.rememberAsyncImagePainter
 import coil3.request.ImageRequest
@@ -43,11 +56,13 @@ fun MovieRow(
     movie: Movie = getMovies()[0],
     onItemClick: (String) -> Unit = {}
 ) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
             .clickable {
                 onItemClick(movie.id)
             },
@@ -69,11 +84,6 @@ fun MovieRow(
                     .transformations(CircleCropTransformation())
                     .build()
                 AsyncImage(model = imageRequest, contentDescription = "Movie Poster")
-
-//                Icon(
-//                    imageVector = Icons.Default.AccountBox,
-//                    contentDescription = "Movie Image"
-//                )
             }
             Column(modifier.padding(4.dp)) {
                 Text(
@@ -87,6 +97,31 @@ fun MovieRow(
                 Text(
                     text = "Released: ${movie.year}",
                     style = MaterialTheme.typography.bodySmall
+                )
+                AnimatedVisibility(visible = expanded) {
+                    Column(
+
+                    ) {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)) {
+                                append("Plot: ")
+                            }
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp, fontWeight = FontWeight.Bold)) {
+                                append(movie.plot)
+                            }
+                        })
+                    }
+                }
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp
+                    else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray
                 )
             }
         }
